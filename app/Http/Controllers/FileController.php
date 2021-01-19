24b98b0aead162ed32afb1c03e9f617333edb9bf
ElijahCoding <cuiyeqing960904@gmail.com>
2021-01-19 23:54:46 +0300

    file endpoints building

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\FileResource;
+use App\Models\File;
 use Aws\S3\PostObjectV4;
 use Aws\S3\S3Client;
 use Illuminate\Http\Request;
@@ -18,6 +19,22 @@ class FileController extends Controller
     public function index(Request $request)
     {
         return FileResource::collection($request->user()->files);
+    }
+
+    public function store(Request $request)
+    {
+        $file = $request->user()->files()
+            ->firstOrCreate(
+                $request->only('path'),
+                $request->only('name', 'size')
+            );
+
+        return new FileResource($file);
+    }
+
+    public function destroy(Request $request, File $file)
+    {
+
     }
 
     public function signed(Request $request)
--- app/Models/File.php
+++ app/Models/File.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class File extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        static::creating(function ($file) {
+            $file->uuid = Str::uuid();
+        });
+    }
 
     public function user()
     {
