80157e7d60c1926a00f29a161afa8ad58a1633b3
ElijahCoding <cuiyeqing960904@gmail.com>
2021-01-19 16:29:16 +0300

    signed upload api built

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\FileResource;
+use Aws\S3\PostObjectV4;
+use Aws\S3\S3Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
@@ -19,8 +22,21 @@ class FileController extends Controller
 
     public function signed(Request $request)
     {
-        $filename = $request->name;
+        $filename = md5($request->name . microtime()) . '.' . $request->extension;
 
-        
+        $object = new PostObjectV4(
+            Storage::disk('s3')->getAdapter()->getClient(),
+            config('services.s3.bucket'),
+            ['key' => 'files/'. $filename],
+            [
+                ['bucket' => config('services.s3.bucket')],
+                ['starts-with', '$key', 'files/']
+            ]
+        );
+
+        return response()->json([
+            'attributes' => $object->getFormAttributes(),
+            'addtionalData' => $object->getFormInputs(),
+        ]);
     }
 }
